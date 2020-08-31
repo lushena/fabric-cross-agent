0a610c19a5ed0e453e399d7b6fd143b28987f52e
lushena <869909600@qq.com>
2020-08-31 17:30:01 +0800

    add invoke interface

--- integration/crossclient/crossclient.go
+++ integration/crossclient/crossclient.go
@@ -6,16 +6,18 @@ import (
 	"github.com/hyperledger/fabric-cross-agent/integration"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
+	"github.com/hyperledger/fabric-sdk-go/pkg/common/providers/fab"
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
 )
 
 type CrossClient struct {
-	FabSDK *fabsdk.FabricSDK
+	FabSDK        *fabsdk.FabricSDK
+	ChannelClient *channel.Client
 }
 
-func NewCrosssClient(sdkOpts ...fabsdk.Option) (*CrossClient, error) {
-	configPath := "/opt/gopath/src/github.com/hyperledger/fabric-cross-agent/config/config_e2e.yaml"
+func NewCrosssClient(channelID, orgName, configPath string, sdkOpts ...fabsdk.Option) (*CrossClient, error) {
+
 	configOpt := config.FromFile(configPath)
 	if integration.IsLocal() {
 		//If it is a local test then add entity mapping to config backend to parse URLs
@@ -27,22 +29,34 @@ func NewCrosssClient(sdkOpts ...fabsdk.Option) (*CrossClient, error) {
 	}
 	c := &CrossClient{}
 	c.FabSDK = fsdk
-	return c, nil
-}
 
-func (cc *CrossClient) Query(ccID, function, channelID, orgName string, args [][]byte, targetEndpoints ...string) ([]byte, error) {
-	clientChannelContext := cc.FabSDK.ChannelContext(channelID, fabsdk.WithUser("Admin"), fabsdk.WithOrg(orgName))
+	clientChannelContext := fsdk.ChannelContext(channelID, fabsdk.WithUser("Admin"), fabsdk.WithOrg(orgName))
 	// Channel client is used to query and execute transactions (Org1 is default org)
 	client, err := channel.New(clientChannelContext)
 	if err != nil {
 		return nil, fmt.Errorf("channel client error:%s", err)
 	}
-	response, err := client.Query(channel.Request{ChaincodeID: ccID, Fcn: function, Args: args},
+	c.ChannelClient = client
+	return c, nil
+}
+
+func (cc *CrossClient) Query(ccID, function string, args [][]byte, targetEndpoints ...string) ([]byte, error) {
+
+	response, err := cc.ChannelClient.Query(channel.Request{ChaincodeID: ccID, Fcn: function, Args: args},
 		channel.WithRetry(retry.DefaultChannelOpts),
 		channel.WithTargetEndpoints(targetEndpoints...),
 	)
 	if err != nil {
-		return nil, fmt.Errorf("query ccid:%s,function:%s, channel:%s, orgname:%s err:%s", ccID, function, channelID, orgName, err)
+		return nil, fmt.Errorf("query ccid:%s,function:%s, err:%s", ccID, function, err)
 	}
 	return response.Payload, err
+}
+
+func (cc *CrossClient) Invoke(ccID, function string, args [][]byte) (fab.TransactionID, error) {
+	response, err := cc.ChannelClient.Execute(channel.Request{ChaincodeID: ccID, Fcn: function, Args: args}, channel.WithRetry(retry.DefaultChannelOpts))
+	if err != nil {
+		return "", fmt.Errorf("invoke ccid:%s,err:%s", ccID, err)
+	}
+
+	return response.TransactionID, nil
 }
--- integration/crossclient/crossclient_test.go
+++ integration/crossclient/crossclient_test.go
@@ -1,6 +1,9 @@
 package crossclient
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 const (
 	channelID      = "mychannel"
@@ -8,23 +11,59 @@ const (
 	orgAdmin       = "Admin"
 	ordererOrgName = "OrdererOrg"
 	peer1          = "peer0.org1.example.com"
+	configPath     = "/opt/gopath/src/github.com/hyperledger/fabric-cross-agent/config/config_e2e.yaml"
 )
 
 var (
 	ccID = "basic"
 )
 
+type Asset struct {
+	ID             string `json:"ID"`
+	Color          string `json:"color"`
+	Size           int    `json:"size"`
+	Owner          string `json:"owner"`
+	AppraisedValue int    `json:"appraisedValue"`
+}
+
 func TestQuery(t *testing.T) {
-	cc, err := NewCrosssClient()
+	cc, err := NewCrosssClient(channelID, orgName, configPath)
 	if err != nil {
 		t.Fatalf("Failed to create crossclient:%s", err)
 	}
 	function := "GetAllAssets"
 	args := [][]byte{[]byte("GetALLAssets")}
-	res, err := cc.Query(ccID, function, channelID, orgName, args)
+	res, err := cc.Query(ccID, function, args)
 	if err != nil {
 		t.Fatal("query err")
 	}
 
 	t.Logf("query res : %s", res)
+}
+
+func TestInvoke(t *testing.T) {
+	cc, err := NewCrosssClient(channelID, orgName, configPath)
+	if err != nil {
+		t.Fatalf("Failed to create crossclient:%s", err)
+	}
+	function := "TransferAsset"
+	args := [][]byte{[]byte("asset6"), []byte("czbank")}
+	res, err := cc.Invoke(ccID, function, args)
+	if err != nil {
+		t.Fatalf("Failed to invoke :%s", err)
+	}
+
+	t.Logf("invoke tx id : %s", res)
+
+	function = "ReadAsset"
+	args = [][]byte{[]byte("asset6")}
+	queryres, err := cc.Query(ccID, function, args)
+	if err != nil {
+		t.Fatalf("Faile to query :%s", err)
+	}
+	asset := Asset{}
+	err = json.Unmarshal(queryres, &asset)
+	if asset.Owner != "czbank" {
+		t.Fatalf("Verify query res failed, query res=%s", queryres)
+	}
 }
